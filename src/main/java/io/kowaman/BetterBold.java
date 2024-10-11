bc4653638303f9b9b15ce10d22c413d139c8b376
Nick <nickinpie@gmail.com>
2024-10-10 23:41:00 -0400

    feat: 1.21.1

--- src/main/java/io/kowaman/BetterBold.java
+++ src/main/java/io/kowaman/BetterBold.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BetterBold implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("assets/betterbold/betterbold");
+	public static final Logger LOGGER = LoggerFactory.getLogger("assets/betterbold/betterbold");
 
 	@Override
 	public void onInitialize() {
